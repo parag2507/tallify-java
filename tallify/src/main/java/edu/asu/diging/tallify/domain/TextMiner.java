7ed0652869f35bef54d4e7a7bf86ad8bc2f61c32
Parag Chaudhari <paragmchaudhari@gmail.com>
2015-11-16 10:11:52 -0700

    C-R002

--- tallify/src/main/java/edu/asu/diging/tallify/domain/TextMiner.java
+++ tallify/src/main/java/edu/asu/diging/tallify/domain/TextMiner.java
@@ -1,27 +1,38 @@
 package edu.asu.diging.tallify.domain;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class TextMiner {
-	
-	Map<String,Integer> getStringCount(String[] text){
-		
-		Map<String,Integer> textMap = new HashMap<String,Integer>();
-		
-		for(String str : text){
-			
-			if(textMap.containsKey(str)){
-				textMap.put(str, textMap.get(str)+1);
-			}
-			
-			else{
-				textMap.put(str, 1);
-			}
-			
+
+	Map<String, Integer> getStringCount(String[] text, String[] ignoreWords) {
+
+		Map<String, Integer> textMap = new HashMap<String, Integer>();
+		Set<String> ignoreWordsSet = new HashSet<String>();
+
+		for (String str : ignoreWords) {
+			ignoreWordsSet.add(str);
+
 		}
-		
+
+		for (String str : text) {
+
+			if (!ignoreWordsSet.contains(str)) {
+				if (textMap.containsKey(str)) {
+					textMap.put(str, textMap.get(str) + 1);
+				}
+
+				else {
+					textMap.put(str, 1);
+				}
+			}
+
+		}
+
 		return textMap;
-		
+
 	}
+
 }
